20a8c3f8310ccdee603c18cf1ebbfd8ca7068186
hackettccp <mhackett@ccp.edu>
2020-01-03 23:25:28 -0500

    Revisions

--- Modules/Module2/CPP/QuickSort/QuickSort.h
+++ Modules/Module2/CPP/QuickSort/QuickSort.h
@@ -1,37 +1,5 @@
 /**
- * Demonstrates the quick sort.
- */
-#include<iostream>
-#include<ctime>
-
-using namespace std;
-
-//Prototypes
-void quicksort(int[], int, int);
-void printArray(int[], int);
-
-int main() {
-    const int LENGTH = 10; //Used to easily change/resize the array for testing with longer or shorter arrays
-    int numbers[LENGTH];
-
-    srand((int)time(0)); //Sets the seed to the current time
-
-    //Fills the array with random numbers. (May include some duplicate numbers.)
-    for(int i = 0; i < LENGTH; i++) {
-          numbers[i] = rand() % 100 + 1; //Puts a random number between 1 and 100 at each index of the array.
-    }
-
-    printArray(numbers, LENGTH); //Prints the current values in the array (Function defined below)
-    cout << "Sorting..." << endl;
-    quicksort(numbers, 0, LENGTH-1);
-    cout << "Done." << endl;
-    printArray(numbers, LENGTH);
-
-    return 0;
-}
-
-/**
- * Quick Sort Algorithm 
+ * Quicksort Algorithm 
  */
 void quicksort(int array[], int start, int end) {
     int lower = start;                                //Index for inserting in the lower partition
@@ -65,15 +33,4 @@ void quicksort(int array[], int start, int end) {
     if(lower < end) {
         quicksort(array, lower, end);                 //Upper partition (Where lower left off through the ending index)
     }
-}
-
-/**
- * Simply prints the current values in the array.
- */
-void printArray(int a[], int length) {
-    cout << "Current values in the array: " << endl;
-    for(int i = 0; i < length; i++) {
-          cout << a[i] << " ";
-    }
-    cout << endl;
 }
