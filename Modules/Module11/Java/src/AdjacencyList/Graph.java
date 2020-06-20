3003c897de9a78ee4f7b551238d343d71a055e86
hackettccp <mhackett@ccp.edu>
2020-06-20 14:28:03 -0400

    Mass Slide Revisions

---
+++ Modules/Module11/Java/src/AdjacencyList/Graph.java
@@ -0,0 +1,4 @@
+package AdjacencyList;
+
+public class Graph {
+}
---
+++ Modules/Module11/Java/src/DirectedGraph/DiGraphDemo.java
@@ -0,0 +1,30 @@
+package DirectedGraphs;
+
+public class GraphDemo {
+
+    public static void main(String[] args) {
+
+        Graph g = new Graph();
+        g.addVertex("A");
+        g.addVertex("B");
+        g.addVertex("C");
+        g.addVertex("D");
+        g.addVertex("E");
+        g.addVertex("F");
+
+        g.addEdge("A", "C");
+        g.addEdge("A", "F");
+        g.addEdge("B", "C");
+        g.addEdge("D", "E");
+        g.addEdge("C", "F");
+
+        g.printGraph();
+
+        System.out.println();
+
+        g.removeEdge("B", "C");
+
+        g.printGraph();
+    }
+
+}
